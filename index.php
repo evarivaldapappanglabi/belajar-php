4f6325be6c6dadb0ace1d04c41c058dcf49478cf
Eva Rivalda Pappang labi <Eva Rivalda Pappang labi>
2022-10-26 15:46:26 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+$nama = "eva";
+
+$umur = 20;
+
+echo "nama saya <strong>$nama</strong>,  saya berusia $umur tahun.<br>";
+
+
+$namaAdik = "lidia";
+$umurAdik = 15;
+
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia$umurAdik tahun.<br>";
+
+
+
+$selisihumur = $umur - $umurAdik;
+
+echo "selisih umur saya dan kakak saya adalah $selisihumur tahun."
+
+?>
